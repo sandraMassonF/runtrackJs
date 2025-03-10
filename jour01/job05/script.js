4f25a4f1a024e9d616f075704c378e55772be500
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-03-10 15:53:48 +0100

    fin job 03, 04 et 05

---
+++ jour01/job05/script.js
@@ -0,0 +1,10 @@
+
+
+let jourSemaines = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+function afficherJoursSemaine() {
+    for (let day of jourSemaines) {
+        console.log(day);
+    }
+}  
+
+console.log(afficherJoursSemaine());
