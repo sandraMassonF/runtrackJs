cc4ab55bb2bdc3e403019bf6c34809ef5d3ce6b7
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-03-11 15:53:30 +0100

    fin job03

---
+++ jour02/job03/script.js
@@ -0,0 +1,9 @@
+
+let compteur = 0;
+
+function addOne(){
+    compteur++;
+    document.getElementById('compteur').textContent = compteur;
+}
+
+document.getElementById('button').addEventListener('click',addOne);
